70a9119820fcdde50032f0b9f4eb8c3f30bf0ff3
Gerwin Fricke <gerwin.fricke@gmail.com>
2023-06-26 16:08:20 +0200

    truncated text and fixes

--- app/page.tsx
+++ app/page.tsx
@@ -227,7 +227,7 @@ export default function Home() {
         ></motion.canvas>
         <div className="h-[60px] bg-[#1D2B3A] fixed bottom-0 z-20 w-full flex flex-row items-center justify-evenly">
           <p className="text-white/80 text-base md:text-lg font-semibold md:leading-[60px] whitespace-nowrap flex flex-row">
-            Case interviews from McKinsey, BCG, Bain, and more
+            Questions from McKinsey, BCG, Bain, and more
           </p>
           {/* <svg
             className="w-[20px] opacity-80"
